004182544781a45d159e56b5c6ac746b27aba615
alpha <sivaji.paladugu@smactechlabs>
2018-04-12 11:11:12 +0000

    modified and add errors.html.erb file

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,7 +20,7 @@ class ArticlesController < ApplicationController
      
      @article = Article.new(article_params)
      if @article.save
-        flash[:notice] = "Article was successfully created"
+        flash[:success] = "Article was successfully created"
         redirect_to article_path(@article)
      else
         render 'new'
@@ -30,7 +30,7 @@ class ArticlesController < ApplicationController
    def update
     
         if @article.update(article_params)
-           flash[:notice] = "Article is updated successfully"
+           flash[:success] = "Article is updated successfully"
            redirect_to article_path(@article)
         else
            render 'edit'
@@ -45,7 +45,7 @@ class ArticlesController < ApplicationController
    def destroy
      
        @article.destroy
-       flash[:notice] = "article is deleted successfully"
+       flash[:danger] = "article is deleted successfully"
        redirect_to articles_path
    end
    
